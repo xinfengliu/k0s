2b84f74c9e0bbf4871fa69762b88581467af4dcb
Karen Almog <kalmog@mirantis.com>
2021-09-02 14:34:50 +0300

    GetYAML -> GetConfigFromAPI
    Signed-off-by: Karen Almog <kalmog@mirantis.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -16,14 +16,62 @@ limitations under the License.
 package config
 
 import (
+	"context"
 	"fmt"
 	"strings"
+	"time"
+
+	cfgClient "github.com/k0sproject/k0s/pkg/apis/k0s.k0sproject.io/clientset"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/k0sproject/k0s/pkg/apis/k0s.k0sproject.io/v1beta1"
 	"github.com/k0sproject/k0s/pkg/constant"
 
 	"github.com/sirupsen/logrus"
 )
+
+var (
+	resourceType = v1.TypeMeta{APIVersion: "k0s.k0sproject.io/v1beta1", Kind: "clusterconfigs"}
+	getOpts      = v1.GetOptions{TypeMeta: resourceType}
+)
+
+func GetConfigFromAPI(kubeConfig string) (*v1beta1.ClusterConfig, error) {
+	timeout := time.After(20 * time.Second)
+	tick := time.Tick(3 * time.Second)
+	// Keep trying until we're timed out or got a result or got an error
+	for {
+		select {
+		// Got a timeout! fail with a timeout error
+		case <-timeout:
+			return nil, fmt.Errorf("timed out waiting for API to return cluster-config")
+		// Got a tick, we should check on doSomething()
+		case <-tick:
+			logrus.Debug("fetching cluster-config from API...")
+			cfg, err := configRequest(kubeConfig)
+			if err != nil {
+				continue
+			}
+			return cfg, nil
+		}
+	}
+}
+
+// fetch cluster-config from API
+func configRequest(kubeConfig string) (clusterConfig *v1beta1.ClusterConfig, err error) {
+	c, err := cfgClient.NewForConfig(kubeConfig)
+	if err != nil {
+		return nil, fmt.Errorf("can't create kubernetes typed client for cluster config: %v", err)
+	}
+	configClient := c.ClusterConfigs(constant.ClusterConfigNamespace)
+	ctxWithTimeout, cancelFunction := context.WithTimeout(context.Background(), time.Duration(10)*time.Second)
+	defer cancelFunction()
+
+	cfg, err := configClient.Get(ctxWithTimeout, "k0s", getOpts)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch cluster-config from API: %v", err)
+	}
+	return cfg, nil
+}
 
 func GetYamlFromFile(cfgPath string, k0sVars constant.CfgVars) (clusterConfig *v1beta1.ClusterConfig, err error) {
 	if cfgPath == "" {
